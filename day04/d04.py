952a1e4a00ee4f276612e540b600593a1e4ab6bb
Lost-Asleep <phammersen@protonmail.com>
2021-12-06 12:58:11 +0100

    Parse input data and basic board class.

--- day04/d04.py
+++ day04/d04.py
@@ -4,53 +4,42 @@ def get_input(fname="./input.txt") -> list[str]:
     return input_data
 
 
-class BingoBoard():
+class BingoBoard:
 
-    def __init__(self, list_of_rows):
-        self.list_of_rows = list(list_of_rows)
+    def __init__(self, board_data: list[str]):
+        self.board_data = list(board_data)
         self.board = dict()
-        print(self.list_of_rows)
+        print(self.board_data)
         # self.create_board()
 
-    def convert_input_to_lists(self):
-        self.input_lists = []
-        for row in self.list_of_rows:
-            row = [num.strip() for num in row]
-            self.input_lists.append(row)
-        
     def create_board(self):
         """x = rows, y = columns"""
         self.board = dict()
-        list_input = self.convert_input_to_lists()
-        print(list_input[0])
-        board_size = len(list_input[0])
+        print(self.board_data[0])
+        board_size = len(self.board_data[0])
         for x in range(board_size):
             for y in range(board_size):
-                self.board[(x, y)] = list_input[x][y]
+                self.board[(x, y)] = self.board_data[x][y]
 
 
-def generate_boards(grid_inpt):
+def parse_board_input(grid_inpt: list[str]) -> list[str]:
+
     data_of_boards = []
     temp = []
-    count = 0
     for line in grid_inpt:
+        line = [x.strip() for x in line.split(" ") if len(x.strip()) > 0] 
+        print(line)
         if len(line) == 0:
             data_of_boards.append(temp)
-            count = 0
             temp = []
             continue
-        count += 1
         temp.append(line)
-        print("Nr of boards:", len(data_of_boards))
-    return [BingoBoard(data) for data in data_of_boards]   
+    data_of_boards.append(temp)
+    return data_of_boards
 
 
 if __name__ == "__main__":
     puzzle_input = get_input()
     drawn_numbers = puzzle_input[0]
-    print(puzzle_input)
-    all_boards = generate_boards(puzzle_input[2:])
-    for board in all_boards:
-        print(len(all_boards))
-        print(board.board)
-    
+    all_boards = parse_board_input(puzzle_input[2:])
+    print(all_boards)
